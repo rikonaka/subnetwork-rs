f8ead47aa608b246bcf6a7c3e3c0091e033d7280
RikoNaka <xxy1836@gmail.com>
2025-05-22 06:00:21 -0400

    Added black_box for tests

--- benches/test_subnetwork/src/main.rs
+++ benches/test_subnetwork/src/main.rs
@@ -1,8 +1,15 @@
+use std::hint::black_box;
+use std::net::Ipv4Addr;
 use subnetwork::Ipv4Pool;
 
-fn main() {
-    let ret = Ipv4Pool::from("192.168.0.0/24").unwrap();
-    for _ in ret {
-        // println!("{}", ip);
+fn my_test() {
+    let ipv4 = Ipv4Addr::new(192, 168, 0, 0);
+    let pool = Ipv4Pool::new(ipv4, 16).unwrap();
+    for _addr in pool {
+        // println!("{}", _addr);
     }
+}
+
+fn main() {
+    black_box(my_test());
 }
